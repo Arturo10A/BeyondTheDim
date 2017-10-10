244c508103e1fbd4fda10aa4edbf2fb555383eab
Arturo Amador Paulino <amadorassassin@gmail.com>
2017-10-10 18:18:29 -0500

    Sterable fisrt version

--- core/src/mx/itesm/beyondthedim/PantallaJuego.java
+++ core/src/mx/itesm/beyondthedim/PantallaJuego.java
@@ -4,11 +4,14 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.ai.steer.Steerable;
+import com.badlogic.gdx.ai.utils.Location;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
@@ -22,7 +25,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
  * Created by Arturo on 05/09/17.
  */
 
-class PantallaJuego extends Pantalla {
+class PantallaJuego extends Pantalla{
 
     private final Juego juego;
     private Texture textureEcenario;  //Imagen del ecenario
@@ -31,10 +34,16 @@ class PantallaJuego extends Pantalla {
     private int pasos = 10;
     private float timerPasos = 0;
 
+    //Jett start
     private  Personaje personaje;
     private int DX_PERSONAJE=10;
     private int DY_PERSONAJE =10;
 
+
+    //Enemy block
+    private Enemy enemy;
+    private int DX_ENEMY = 10;
+    private int DY_ENMEY = 10;
 
     private Stage escenaJuego;
     private Texture texturaBtnGoBack; //Boton de regreso
@@ -46,20 +55,6 @@ class PantallaJuego extends Pantalla {
 
     private Texto texto;
 
-
-    //Method to touchpad
-
-    private Touchpad touchpad;
-    private Touchpad.TouchpadStyle touchpadStyle;
-    private Skin touchpadSkin;
-    private Drawable touchBackground;
-    private Drawable touchKnob;
-    private Texture blockTexture;
-    private Sprite blockSprite;
-    private float blockSpeed;
-
-
-
     public PantallaJuego(Juego juego) {
 
 
@@ -68,23 +63,12 @@ class PantallaJuego extends Pantalla {
 
     public void cargarTexturas(){
         texturaBtnGoBack = new Texture("button_pause.png");
-        textureJoistickDerecho = new Texture("joi.png");
-        textureJoistickIzquierdo = new Texture("joi.png");
 
     }
 
     public void crearEscena(){
         escenaJuego = new Stage(vista);
 
-
-        TextureRegionDrawable trdIZQ = new TextureRegionDrawable(new TextureRegion(textureJoistickIzquierdo));
-        ImageButton btnIZQ = new ImageButton(trdIZQ);
-        btnIZQ.setPosition(200,100);
-
-
-        TextureRegionDrawable trdDer = new TextureRegionDrawable(new TextureRegion(textureJoistickDerecho));
-        ImageButton btnDerc = new ImageButton(trdDer);
-        btnDerc.setPosition(1000,100);
 
         //Boton GOBACK
 
@@ -94,8 +78,7 @@ class PantallaJuego extends Pantalla {
         btnGoBack.setPosition(ANCHO-btnGoBack.getWidth()-5,ALTO-btnGoBack.getHeight()-5);
 
         escenaJuego.addActor(btnGoBack);
-        escenaJuego.addActor(btnIZQ);
-        escenaJuego.addActor(btnDerc);
+
         btnGoBack.addListener(new ClickListener(){
 
             @Override
@@ -111,6 +94,8 @@ class PantallaJuego extends Pantalla {
 
     }
 
+
+
     @Override
     public void show() {
 
@@ -119,6 +104,9 @@ class PantallaJuego extends Pantalla {
 
         textureEcenario = new Texture("inicio.png");
         personaje = new Personaje(ANCHO/4,ALTO/2);
+
+        //Class enemy test
+        enemy = new Enemy(0, 0, 100);
 
         Gdx.input.setInputProcessor(escenaJuego);
         Gdx.input.setInputProcessor(new ProcesadorEventos());
@@ -136,9 +124,11 @@ class PantallaJuego extends Pantalla {
 
         batch.draw(textureEcenario,Pantalla.ANCHO/2-textureEcenario.getWidth()/2, Pantalla.ALTO/2-textureEcenario.getHeight()/2);
 
-        //Dibujar personaje
+        //Drawing jett
         personaje.render(batch);
 
+        //Drawing test of class enemy
+        enemy.render(batch);
 
         texto.mostrarMensaje(batch,"Vida: 100%",50,Pantalla.ALTO/1.02f);
 
@@ -154,10 +144,9 @@ class PantallaJuego extends Pantalla {
 
     }
 
-
     @Override
     public void pause() {
-
+        
     }
 
     @Override
@@ -170,7 +159,8 @@ class PantallaJuego extends Pantalla {
 
     }
 
-   private class ProcesadorEventos implements InputProcessor {
+
+    private class ProcesadorEventos implements InputProcessor {
 
         @Override
         public boolean keyDown(int keycode) {
